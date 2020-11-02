30b3442a7fa3d0947e93fe91da42753a99204374
Connor Bettermann <connorbettermann@gmail.com>
2020-11-02 11:53:09 -0800

    force news card size for consistency

--- src/components/News.js
+++ src/components/News.js
@@ -9,7 +9,7 @@ const News = (props) => {
         <div className="col s6">
             <div className="card hoverable"><a href={props.url} target="__blank" rel="noopener">
                 <div className="card-image waves-effect waves-block waves-light">
-                    <img src="https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianBLACK.png" alt="thumbnail" height="320px"></img>
+                    <img src="https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianWHITE.png" alt="thumbnail" height="320px"></img>
                     <span className="card-title blue-grey darken-3 truncate">{props.title}</span>
                 </div>
                </a>
@@ -23,7 +23,7 @@ const News = (props) => {
             <div className="card hoverable"><a href={props.url} target="__blank" rel="noopener">
                 <div className="card-image waves-effect waves-block waves-light">
                     {
-                        props.image == null ? <img src={`https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianREV.png`} alt="thumbnail"/> : <img src={`${props.image}`} alt="thumbnail"/>
+                        props.image == null ? <img src={`https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianWHITE.png`} alt="thumbnail"/> : <img src={`${props.image}`} alt="thumbnail"/>
                     }
                     <span className={`card-title blue-grey darken-3 ${isTruncated ? "truncate" : ""}`} onMouseEnter={() => setIsTruncated(false)} onMouseLeave={() => setIsTruncated(true)}>{props.title}</span>
                 </div>
