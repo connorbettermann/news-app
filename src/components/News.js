dcfe02f7b39bb3a3a3425039423cfbe11299552f
Connor Bettermann <connorbettermann@gmail.com>
2020-11-01 22:13:45 -0800

    changed height of stock image cards

--- src/components/News.js
+++ src/components/News.js
@@ -9,7 +9,7 @@ const News = (props) => {
         <div className="col s6">
             <div className="card hoverable"><a href={props.url} target="__blank" rel="noopener">
                 <div className="card-image waves-effect waves-block waves-light">
-                    <img src="https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianBLACK.png" alt="thumbnail" height="282.75px"></img>
+                    <img src="https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianBLACK.png" alt="thumbnail" height="320px"></img>
                     <span className="card-title blue-grey darken-3 truncate">{props.title}</span>
                 </div>
                </a>
@@ -23,7 +23,7 @@ const News = (props) => {
             <div className="card hoverable"><a href={props.url} target="__blank" rel="noopener">
                 <div className="card-image waves-effect waves-block waves-light">
                     {
-                        props.image == null ? <img src={`https://www.google.com/url?sa=i&url=https%3A%2F%2Fkahoot.com%2Fkahoot-news%2F25-best-video-games-to-help-you-socialise-while-self-isolating%2F&psig=AOvVaw3Zuw8___sL8XCtC95_cRww&ust=1604351831161000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDKhu-v4uwCFQAAAAAdAAAAABAD`} alt="thumbnail"/> : <img src={`${props.image}`} alt="thumbnail"/>
+                        props.image == null ? <img src={`https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/03/01/poweredbyguardianREV.png`} alt="thumbnail"/> : <img src={`${props.image}`} alt="thumbnail"/>
                     }
                     <span className={`card-title blue-grey darken-3 ${isTruncated ? "truncate" : ""}`} onMouseEnter={() => setIsTruncated(false)} onMouseLeave={() => setIsTruncated(true)}>{props.title}</span>
                 </div>
