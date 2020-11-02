57e25f88739d8e781594efd62132b53fcbbb1cae
Connor Bettermann <connorbettermann@gmail.com>
2020-11-02 11:58:07 -0800

    changed hrefs to stay on current page placement. stickied navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,17 +3,19 @@ import React from 'react';
 const Nav = () => {
     //Display Nav bar with logo
     return (
-        <nav className="blue-grey darken-3">
-            <div className="nav-wrapper container">
-                <a href="https://www.theguardian.com/us" target="__blank" rel="noopener" className="brand-logo"><b>The Guardian</b> News Feed</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="https://github.com/connorbettermann/news-app" target="__blank" rel="noopener">Source</a></li>
-                    <li><a href="https://connorbettermann.github.io/" target="__blank" rel="noopener">Portfolio</a></li>
-                    <li><a href="https://reactjs.org/docs/getting-started.html" target="__blank" rel="noopener">React Docs</a></li>
-                    <li><a href="https://materializecss.com/getting-started.html" target="__blank" rel="noopener">Materialize Docs</a></li>
-                </ul>
-            </div>
-        </nav>
+        <div className="navbar-fixed">
+            <nav className="blue-grey darken-3">
+                <div className="nav-wrapper container">
+                    <a href="https://www.theguardian.com/us" target="__blank" rel="noopener" className="brand-logo"><b>The Guardian</b> News Feed</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="https://github.com/connorbettermann/news-app" target="__blank" rel="noopener">Source</a></li>
+                        <li><a href="https://connorbettermann.github.io/" target="__blank" rel="noopener">Portfolio</a></li>
+                        <li><a href="https://reactjs.org/docs/getting-started.html" target="__blank" rel="noopener">React Docs</a></li>
+                        <li><a href="https://materializecss.com/getting-started.html" target="__blank" rel="noopener">Materialize Docs</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
 
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,7 +12,7 @@ const Pagination = (props) => {
         //update pagination so that 5 pages are shown on either side of current page
         for(let i = props.currentPage - 5; i <= props.currentPage + 5; i++){
             let active = props.currentPage === i ? 'active' : '';    
-            pageLinks.push(<li className={`waves-effect ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#top">{i}</a></li>)
+            pageLinks.push(<li className={`waves-effect ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#!">{i}</a></li>)
         }
     } else {
     
@@ -20,7 +20,7 @@ const Pagination = (props) => {
     for(let i = 1; i <= 10; i++) {
         let active = props.currentPage === i ? 'active' : '';
 
-        pageLinks.push(<li className={`waves-effect ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#top">{i}</a></li>)
+        pageLinks.push(<li className={`waves-effect ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#!">{i}</a></li>)
     }
     }
 
@@ -34,9 +34,9 @@ const Pagination = (props) => {
         <div className="container">
             <div className="row">
                 <ul className="pagination center-align">
-                    { props.currentPage > 1 ? <li className={`waves-effect`} onClick={() => props.nextPage(props.currentPage - 1)}><a href="#top">Prev</a></li> : ''}
+                    { props.currentPage > 1 ? <li className={`waves-effect`} onClick={() => props.nextPage(props.currentPage - 1)}><a href="#!">Prev</a></li> : ''}
                     { pageLinks }
-                    { props.currentPage < props.pages ? <li className={`waves-effect`} onClick={() => props.nextPage(props.currentPage + 1)}><a href="#top">Next</a></li> : ''}
+                    { props.currentPage < props.pages ? <li className={`waves-effect`} onClick={() => props.nextPage(props.currentPage + 1)}><a href="#!">Next</a></li> : ''}
                 </ul>
             </div>
         </div>
