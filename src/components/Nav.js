e5da0deec4e0e29f9dc7a8436cc1a94a28f5d6b8
Connor Bettermann <connorbettermann@gmail.com>
2020-11-02 10:34:26 -0800

    added formatting and links to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,13 @@ const Nav = () => {
     return (
         <nav className="blue-grey darken-3">
             <div className="nav-wrapper container">
-                <a href="https://www.theguardian.com/us" target="__blank" rel="noopener" className="brand-logo">The Guardian News Feed</a>
+                <a href="https://www.theguardian.com/us" target="__blank" rel="noopener" className="brand-logo"><b>The Guardian</b> News Feed</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="https://github.com/connorbettermann/news-app" target="__blank" rel="noopener">Source</a></li>
+                    <li><a href="https://connorbettermann.github.io/" target="__blank" rel="noopener">Portfolio</a></li>
+                    <li><a href="https://reactjs.org/docs/getting-started.html" target="__blank" rel="noopener">React Docs</a></li>
+                    <li><a href="https://materializecss.com/getting-started.html" target="__blank" rel="noopener">Materialize Docs</a></li>
+                </ul>
             </div>
         </nav>
     )
